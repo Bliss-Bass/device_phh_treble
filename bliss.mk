c299f67af576b4e8dd788df12720da073e1abf33
Jon West <electrikjesus@gmail.com>
2018-06-13 19:05:46 -0400

    Add generated files

--- bliss.mk
+++ bliss.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, vendor/bliss/config/common.mk)
+$(call inherit-product, device/bliss/sepolicy/common/sepolicy.mk)
 
 #By default, CarbonROM copies it into root/init.carbon.rc
 #For this to work in GSI, we put it in /system
---
+++ vendorsetup.sh
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+add_lunch_combo aosp_arm64_a-userdebug
+add_lunch_combo aosp_arm64_ab-userdebug
+add_lunch_combo bliss_arm64_a-userdebug
+add_lunch_combo bliss_arm64_a-userdebug
+add_lunch_combo bliss_arm64_ab-userdebug
+add_lunch_combo bliss_arm64_ab-userdebug
+add_lunch_combo treble_arm64_avN-userdebug arm64-aonly-vanilla-nosu
+add_lunch_combo treble_arm64_agS-userdebug arm64-aonly-gapps-su
+add_lunch_combo treble_arm64_bvN-userdebug arm64-ab-vanilla-nosu
+add_lunch_combo treble_arm64_bgS-userdebug arm64-ab-gapps-su
+add_lunch_combo treble_arm_avN-userdebug arm-aonly-vanilla-nosu
