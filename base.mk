8dfe61e97dd713c784d7ff351f4d28284539d7c1
Jon West <electrikjesus@gmail.com>
2018-06-17 03:35:13 +0530

    Update 'base.mk'

--- base.mk
+++ base.mk
@@ -61,3 +61,5 @@ $(call inherit-product, vendor/bliss/config/common.mk)
 
 PRODUCT_COPY_FILES += \
 	vendor/bliss/prebuilt/common/etc/init.bliss.rc:system/etc/init/init.bliss.rc
+
+BLISS_BUILDTYPE := OFFICIAL
