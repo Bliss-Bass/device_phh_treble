2f64f4f498b42401aeeb9032880e3b652ba89696
Jon West <electrikjesus@gmail.com>
2018-06-16 23:04:48 +0530

    remove custom sepolicy

--- bliss.mk
+++ bliss.mk
@@ -1,5 +1,4 @@
 $(call inherit-product, vendor/bliss/config/common.mk)
-$(call inherit-product, device/bliss/sepolicy/common/sepolicy.mk)
 
 #By default, CarbonROM copies it into root/init.carbon.rc
 #For this to work in GSI, we put it in /system
